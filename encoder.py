1e95303169677d1564f75cc01f60ebdd8a3946cd
fethi <fethiourghi@gmail.com>
2021-04-07 00:10:01 +0100

    stable commit

---
+++ encoder.py
@@ -0,0 +1,64 @@
+#!flask/bin/python
+
+from facerecognizer import *
+import numpy as np
+import cv2
+
+from flask import Flask, jsonify,request,Response
+import jsonpickle
+
+import requests
+
+
+DB_URL = "http://127.0.0.1:8000/personapp/persons/"
+
+app = Flask(__name__)
+
+def save_person(data):
+    response = requests.post(DB_URL, json=data)
+    print(response)
+
+
+def recognize_face_image(image):
+    rectangles = face_locations(image)
+    response = {
+        'result': 'none or several faces in the input images'
+    }
+    encoded = False
+    if len(rectangles) == 1:
+        t, r, b, l = rectangles[0]
+        face = image[t:b, l:r]
+        encoding = face_encodings(face)
+        if len(encoding)!=0:
+            response['result'] = encoding[0].tolist()
+            encoded = True
+    return response, encoded
+
+
+# route http posts to this method
+@app.route('/encode/', methods=['POST'])
+def inference():
+    r = request
+    uploaded_img = r.files['media']
+    nparr = np.frombuffer(uploaded_img.read(), dtype=np.uint8)
+    img = cv2.imdecode(nparr, flags=1)
+    result, encoded = recognize_face_image(img)
+    
+    response = jsonpickle.encode(result)
+    if encoded:
+        data = {
+            'permission': r.values['permission'],
+            'first_name': r.values['first_name'],
+            'last_name': r.values['last_name'],
+            'photo_path': '/profile.png',
+            'encoding': str(result['result'])
+        }
+
+        save_person(data)
+
+    return Response(response=response, status=200, mimetype="application/json")
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
